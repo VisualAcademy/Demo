26effc85cdd47679cd4e5423d8ebd99627366b3f
RedPlus <redplus@redplus.net>
2019-06-25 05:18:30 +0900

    gRPC 서버와 클라이언트 쪽 코드 구현

---
+++ RpcService.Server/Services/ArithmeticServer.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Arithmetic;
+using Grpc.Core;
+
+namespace RpcService.Server
+{
+    public class ArithmeticServer : ArithmeticService.ArithmeticServiceBase
+    {
+        public override Task<ResponseMessage> Add(RequestMessage request, ServerCallContext context)
+        {
+            var r = new ResponseMessage();
+            r.Result = request.First + request.Second;
+            return Task.FromResult(r);
+        }
+
+        public override Task<ResponseMessage> Substract(RequestMessage request, ServerCallContext context)
+        {
+            var r = new ResponseMessage();
+            r.Result = request.First - request.Second;
+            return Task.FromResult(r);
+        }
+    }
+}
